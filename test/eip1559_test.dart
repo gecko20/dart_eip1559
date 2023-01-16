3221ff4508776a599d03e7d46214efd96da33f66
Mahdi K. Fard <mkfard@outlook.com>
2023-01-16 16:27:32 +0330

    getBlockByNumber test.

--- test/eip1559_test.dart
+++ test/eip1559_test.dart
@@ -2,16 +2,45 @@ import 'dart:io';
 import 'package:eip1559/eip1559.dart';
 import 'package:test/test.dart';
 
-final infuraId = Platform.environment["INFURA_ID"];
+final infuraId = Platform.environment["INFURA_ID"] ?? '';
 
 void main() {
   final url = 'https://mainnet.infura.io/v3/$infuraId';
-  test('Gas Estimation', () async {
-    final rates = await getGasInEIP1559(
-      url,
-      block: '0xfa8925',
-    );
 
-    expect(rates[2].estimatedGas.toString(), '15363531999');
-  });
+  test(
+    'getBlockByNumber',
+    () async {
+      final blockInfo = await getBlockByNumber(
+        url,
+        '0xd6c34e',
+      );
+
+      expect(
+        blockInfo.timestamp.millisecondsSinceEpoch == 1643113026000,
+        isTrue,
+      );
+      expect(
+        blockInfo.timestamp.isUtc == true,
+        isTrue,
+      );
+    },
+    skip: infuraId.isEmpty
+        ? 'Tests require the INFURA_ID environment variable'
+        : null,
+  );
+
+  test(
+    'Gas Estimation',
+    () async {
+      final rates = await getGasInEIP1559(
+        url,
+        block: '0xfa8925',
+      );
+
+      expect(rates[2].estimatedGas.toString(), '15363531999');
+    },
+    skip: infuraId.isEmpty
+        ? 'Tests require the INFURA_ID environment variable'
+        : null,
+  );
 }
